284d5066e7b3c20e8f6cd5d2b205b6b092960d7c
George Bell <george@cattlebell.com>
2022-12-05 08:04:48 +0000

    Create part1.py

---
+++ day5/part1.py
@@ -0,0 +1,36 @@
+input = ['    [D]    ','[N] [C]    ','[Z] [M] [P] ',' 1   2   3 ']
+
+instructions = ['move 1 from 2 to 1','move 3 from 1 to 3','move 2 from 2 to 1','move 1 from 1 to 2']
+
+stack_values = range(0, int(input[-1][-2]))
+stacks = []
+for stack in stack_values:
+  stacks.append([])
+
+input.pop(-1)
+
+for line in input:
+  name_index = 1
+  stack_index = 0
+  for stack in stack_values:
+    if line[name_index] != ' ':
+      stacks[stack_index].append(line[name_index])
+    name_index += 4
+    stack_index += 1
+
+for task in instructions:
+  split_task = task.split(' ')
+  number_to_move = range(0, int(split_task[1]))
+  move_from = int(split_task[3]) - 1
+  move_to = int(split_task[5]) - 1
+
+  for x in number_to_move:
+    item_to_move = stacks[move_from].pop(0)
+    stacks[move_to].insert(0, item_to_move)
+
+output = ''
+
+for stack in stacks:
+  output += stack[0]
+
+print(output)
