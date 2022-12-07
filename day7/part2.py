395e1b6943acb3414d02841718bf1c357f14ae77
George Bell <george@cattlebell.com>
2022-12-07 09:44:00 +0000

    Create part2.py

---
+++ day7/part2.py
@@ -0,0 +1,76 @@
+class Node:
+  def __init__(self, name, parent, size):
+    self.name = name
+    self.parent = parent
+    self.size = size
+    self.children = []
+
+  def calculate_size(self):
+    if self.size != 0:
+      return self.size
+    size = 0
+    for child in self.children:
+      size += child.calculate_size()
+    return size
+
+  def add_child(self, child):    
+    self.children.append(child)
+
+  def get_parent(self):
+    return self.parent
+
+  def get_name(self):
+    return self.name
+
+  def get_child(self, child_name):
+    for child in self.children:
+      if child.get_name() == child_name:
+        return child
+
+  def is_dir(self):
+    return True if self.size == 0 else False
+
+  def calculate_dir_sizes(self):
+    sizes = []
+    for child in self.children:
+      if child.is_dir():
+        sizes += child.calculate_dir_sizes()
+    if self.is_dir:
+      sizes.append((self.name, self.calculate_size()))
+    return sizes
+
+input = ['$ cd /','$ ls','dir a','14848514 b.txt','8504156 c.dat','dir d','$ cd a','$ ls','dir e','29116 f','2557 g','62596 h.lst','$ cd e','$ ls','584 i','$ cd ..','$ cd ..','$ cd d','$ ls','4060174 j','8033020 d.log','5626152 d.ext','7214296 k']
+
+starting_node = Node('/', None, 0)
+current_node = starting_node
+
+for line in input:
+  split_line = line.split(' ')
+  if line[0] == '$':
+    if split_line[1] == 'cd':
+      if split_line[2] == '/':
+        current_node = starting_node
+      elif split_line[2] == '..':
+        current_node = current_node.get_parent()
+      else:
+        current_node = current_node.get_child(split_line[2])
+  else:
+    if split_line[0] == 'dir':
+      new_dir = Node(split_line[1], current_node, 0)
+      current_node.add_child(new_dir)
+    else:
+      new_file = Node(split_line[1], current_node, int(split_line[0]))
+      current_node.add_child(new_file)
+
+disk_space = 70000000
+current_free_space = disk_space - starting_node.calculate_size()
+dir_sizes = starting_node.calculate_dir_sizes()
+deletion_candidates = []
+for size in dir_sizes:
+  potential_free_space = current_free_space + size[1]
+  if potential_free_space >= 30000000:
+    deletion_candidates.append(size[1])
+
+deletion_candidates.sort()
+
+print(deletion_candidates[0])
